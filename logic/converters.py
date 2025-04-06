50df87c05b5ead709ac00e91d43c0fad041e23b8
Me <me@me.com>
2025-04-06 19:55:19 +0400

    Новый тест для абстрактного метода стратегии
    Проверка, что convert - абстрактный метод

--- logic/converters.py
+++ logic/converters.py
@@ -1,6 +1,7 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class ConversionStrategy(ABC):
+    @abstractmethod
     def convert(self):
         pass
--- logic/tests/test_interfaces.py
+++ logic/tests/test_interfaces.py
@@ -1,14 +1,10 @@
 # logic/tests/test_interfaces
 from abc import ABC
 from unittest import TestCase
-
 from logic.converters import ConversionStrategy
 
 
 class StrategyInterfaceTest(TestCase):
-    def setUp(self):
-        self.abstract_strategy = ConversionStrategy()
-
     def test_abstract_strategy_interface_exists(self):
         '''
         Тест: Существование базового интерфейса стратегии
@@ -23,17 +19,27 @@ class StrategyInterfaceTest(TestCase):
         '''
         Тест: Класс стратегии наследуется от ABC
         '''
-        self.assertIsInstance(self.abstract_strategy, ABC, 'Класс не наследуется от ABC')
+        self.assertTrue(issubclass(ConversionStrategy, ABC), 'Класс не наследуется от ABC')
 
     def test_abstract_strategy_class_has_convert_method(self):
         '''
         Тест: Проверка инициализации абстрактного метода convert
         '''
         self.assertTrue(
-            hasattr(self.abstract_strategy, 'convert'),
+            hasattr(ConversionStrategy, 'convert'),
             'Метода convert не существует для ConversionStrategy'
             )
         self.assertTrue(
-            callable(getattr(self.abstract_strategy, 'convert')),
+            callable(getattr(ConversionStrategy, 'convert')),
             'Не инициализрован метод convert для базового ConversionStrategy'
         )
+
+    def test_convert_method_is_abstract(self):
+        '''
+        Тест: ConversionStrategy имеет декоратор abstractmethod для метода convert
+        '''
+        convert_method = getattr(ConversionStrategy, 'convert')
+        self.assertTrue(
+            getattr(convert_method, '__isabstractmethod__', False),
+             "Метод convert должен быть помечен как @abstractmethod"
+             )
